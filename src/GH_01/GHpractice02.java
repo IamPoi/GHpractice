705d756120afddac68edc6163c4d4b627195f734
smhrd <smhrd@121.147.185.66>
2021-03-14 18:43:21 +0900

    num 추가

--- src/GH_01/GHpractice02.java
+++ src/GH_01/GHpractice02.java
@@ -3,6 +3,8 @@ package GH_01;
 public class GHpractice02 {
 	public static void main(String[] args) {
 		System.out.println("Github Connecting");
+		int num = 1;
+		System.out.println(num);
 	}
 
 }
